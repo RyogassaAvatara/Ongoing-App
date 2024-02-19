37fd9a4658170584525bdcc7eb6a3d673696bde5
RyogassaAvatara <RyogassaAvatara@users.noreply.github.com>
2024-02-19 22:43:19 +0700

    first commit

---
+++ src/app/notes/NavBar.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+import logo from "@/assets/logo.png";
+import { UserButton } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
+import { useState } from "react";
+import AddNoteInfo from "@/components/AddNoteInfo";
+
+export default function NavBar() {
+    const [showAddNoteInfo, setShowAddNoteInfo] = useState(false);
+
+    return (
+        <>
+        <div className="p-4 shadow"> 
+            <div className="max-w-7xl m-auto flex flex-wrap gap-3 items-center justify-between">
+                <Link href="/notes" className="flex items-center gap-3">
+                    <Image src={logo} alt="IntelliNote Logo" width={40} height={40} />
+                    <span className="font-bold">IntelliNotes</span>
+                </Link>
+                <div className="flex items-center gap-2">
+                    <UserButton afterSignOutUrl="/"
+                    appearance={{
+                        elements: { avatarBox: { width: "2.5rem", height:"2.5rem" } }
+                    }}
+                    />
+                    <Button onClick={() => setShowAddNoteInfo(true)}>
+                        <Plus size={20} className="mr-2" />
+                        Add Note
+                    </Button>
+                </div>
+            </div>
+        </div>
+        <AddNoteInfo open={showAddNoteInfo} setOpen={setShowAddNoteInfo} />
+        </>
+    );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,31 @@
+import Image from 'next/image'
+import logo from "@/assets/logo.png";
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+import { auth } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
+
+
+export default function Home() {
+  const {userId} = auth();
+
+  if (userId) redirect("/notes");
+
+  return (
+    <main className='flex flex-col h-screen items-center justify-center gap-5'>
+      <div className='flex items-center gap-5'>
+        <Image src={logo} alt='IntelliNote' width={100} height={100} />
+        <span className='font-extrabold tracking-tight text-4xl lg:text-5xl'>
+          IntelliNotes
+        </span>
+      </div>
+      <p className='text-center max-w-prose'>
+      Transform your notes app with a custom AI chatbot, 
+      tailored from your saved notes, offering insightful assistance to boost productivity.
+      </p>
+      <Button size="lg" asChild>
+        <Link href="/notes">Open</Link>
+      </Button>
+    </main>
+  )
+}
